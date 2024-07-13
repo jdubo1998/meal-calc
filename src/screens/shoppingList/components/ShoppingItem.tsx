8ed45f2dc3ad819d52868f8d236e05a7d4a6edf8
Jacob DuBois <jdubo1998@gmail.com>
2024-07-12 22:06:03 -0500

    Created check box for shopping list items.
    Shoppint List
    - A check box appears next to each item on the shopping list.

--- src/screens/shoppingList/components/ShoppingItem.tsx
+++ src/screens/shoppingList/components/ShoppingItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 type ShoppingItemProps = {
@@ -9,17 +10,35 @@ type ShoppingItemProps = {
 };
 
 function ShoppingItem(props: ShoppingItemProps) {
+    const [selected, setSelected] = useState(false);
+
     return (
-        <View style={styles.shoppingitem}>
-            <View style={styles.shoppingitembar}>
-                <Text style={[styles.whitetext, {flex: 1}]}>X</Text>
-                <Text style={[styles.whitetext, {flex: 10}]}>{props.name}</Text>
-                <Text style={[props.critical ? styles.redtxt : styles.whitetext, {flex: 3, textAlign: 'right', paddingEnd: 10}]}>{`$${props.cost}`}</Text>
+        <View style={styles.shoppingitembar}>
+            {/* Checkbox */}
+            <View style={[styles.checkbox]}>
+                <Text style={styles.checkboxtext} onPress={ () => {
+                    if (!selected) {
+                        // TODO: Open the modal to add item to receipt.
+
+                        console.log(`Bought: ${props.name}`);
+                    }
+
+                    setSelected(!selected);
+                } }>{selected ? 'X' : ''}</Text>
             </View>
-            <View style={styles.shoppingitembar}>
-                <View style={{flex: 1}}/>
-                <Text style={[styles.smallgreytext, {flex: 10}]}>{props.store}</Text>
-                <Text style={[props.critical ? styles.redtxt : styles.smallgreytext, {flex: 3, textAlign: 'right', paddingEnd: 10}]}>{props.count}</Text>
+
+            <View style={styles.shoppingitem}>
+                {/* Item and Price */}
+                <View style={styles.shoppingitembar}>
+                    <Text style={[styles.whitetext, {flex: 10}]}>{props.name}</Text>
+                    <Text style={[props.critical ? styles.redtxt : styles.whitetext, {flex: 3, textAlign: 'right', paddingEnd: 10}]}>{`$${props.cost}`}</Text>
+                </View>
+
+                {/* Store and Quantity */}
+                <View style={styles.shoppingitembar}>
+                    <Text style={[styles.smallgreytext, {flex: 10}]}>{props.store}</Text>
+                    <Text style={[props.critical ? styles.redtxt : styles.smallgreytext, {flex: 3, textAlign: 'right', paddingEnd: 10}]}>{props.count}</Text>
+                </View>
             </View>
         </View>
     );
@@ -27,13 +46,28 @@ function ShoppingItem(props: ShoppingItemProps) {
 
 const styles = StyleSheet.create({
     shoppingitem: {
-        flexDirection: 'column'
+        flexDirection: 'column',
+        flex: 15
     },
     shoppingitembar: {
         flexDirection: 'row',
         paddingStart: 10,
         paddingEnd: 10
     },
+
+    checkbox: {
+        justifyContent: 'center',
+        marginVertical: 5,
+        borderWidth: 3,
+        borderColor: '#FFFFFF',
+        flex: 1
+    },
+    checkboxtext: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        textAlign: 'center'
+    },
+
     redtxt: {
         color: '#AA0000',
         fontSize: 15
